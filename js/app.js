683ab52b14eb5827ff53e25ff838665456397dcd
AnanSer <ananserbesa@gmail.com>
2024-11-24 17:17:06 +0300

    updated navbar with js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,26 @@ const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
+
 const date = document.querySelector("#date");
 const aboutBtn = document.querySelector("#about-btn");
-const projects = document.querySelector("#projects");
-const about = document.querySelector(".about");
-const contact = document.querySelector(".contact");
+const heroBtn = document.querySelector(".hero-btn");
+// const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Add smooth scrolling to each link
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default anchor click behavior
+    const targetId = this.getAttribute("href").substring(1); // Get the target section ID
+    const targetSection = document.getElementById(targetId);
+
+    // Scroll to the target section smoothly
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  });
+});
 
 // const
 // add fixed class to navbar
@@ -29,17 +44,8 @@ aboutBtn.addEventListener("click", function () {
   projects.scrollIntoView({ behavior: "smooth" });
 });
 
-about.addEventListener("click", function () {
-  about.scrollIntoView({ behavior: "smooth" });
+heroBtn.addEventListener("click", function () {
+  contact.scrollIntoView({ behavior: "smooth" });
 });
 
-// projects.addEventListener("click", function () {
-//   projects.scrollIntoView({ behavior: "smooth" });
-// });
-
-// contact.addEventListener("click", function () {
-//   contact.scrollIntoView({ behavior: "smooth" });
-// });
-
-// set year
 date.innerHTML = new Date().getFullYear();
