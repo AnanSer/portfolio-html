a8bc58f292fa6ff10976dccf7dbcf6cae275d736
AnanSer <ananserbesa@gmail.com>
2024-11-24 14:47:42 +0300

    updated the index

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const aboutBtn = document.querySelector("#about-btn");
+const projects = document.querySelector("#projects");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
+
+// const
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -18,5 +24,22 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
+aboutBtn.addEventListener("click", function () {
+  projects.scrollIntoView({ behavior: "smooth" });
+});
+
+about.addEventListener("click", function () {
+  about.scrollIntoView({ behavior: "smooth" });
+});
+
+// projects.addEventListener("click", function () {
+//   projects.scrollIntoView({ behavior: "smooth" });
+// });
+
+// contact.addEventListener("click", function () {
+//   contact.scrollIntoView({ behavior: "smooth" });
+// });
+
 // set year
 date.innerHTML = new Date().getFullYear();
